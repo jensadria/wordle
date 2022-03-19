d50640ba927b486cd923925b009011d7d50f4d98
Jens Adria <jensadria@gmail.com>
2022-03-19 18:03:37 +0800

    Added functionality to keyboard - backspace

--- script.js
+++ script.js
@@ -4,36 +4,40 @@ const wordGuessField = document.getElementById('guess');
 const randomWordIndex = Math.floor(Math.random() * validWords.length);
 const randomWord = validWords[randomWordIndex];
 
-const allRows = document.getElementsByClassName('row');
-//const rows = document.querySelectorAll('.container > div');
-
 let guessNr = 0;
 let currentGuess = [];
 const currentRow = document.querySelectorAll(`#row-${guessNr} >.box`);
 
-wordGuessForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const wordGuess = wordGuessField.value;
+//wordGuessForm.addEventListener('submit', (e) => {
+//  e.preventDefault();
+//  const wordGuess = wordGuessField.value;
 
-  //  const rowBoxes = document.querySelectorAll('#row-1 >.box');
+//  for (const index in rowBoxes) {
+//    const rowBox = rowBoxes[index];
+//    rowBox.textContent = wordGuess[index];
 
-  for (const index in rowBoxes) {
-    const rowBox = rowBoxes[index];
-    rowBox.textContent = wordGuess[index];
+//    if (wordGuess[index] === randomWord[index]) {
+//      rowBox.className = 'box correct-spot';
+//    } else if (randomWord.includes(wordGuess[index])) {
+//      rowBox.className = 'box wrong-spot';
+//    }
+//  }
+//});
 
-    if (wordGuess[index] === randomWord[index]) {
-      rowBox.className = 'box correct-spot';
-    } else if (randomWord.includes(wordGuess[index])) {
-      rowBox.className = 'box wrong-spot';
-    }
-  }
-});
+function updateRow(row, word) {
+  row.forEach((letterBox, index) => {
+    letterBox.textContent = word[index];
+  });
+
+  //	let index = word.length - 1;
+  //  row[index].textContent = word[index];
+}
 
 const keyRows = {
   rowOne: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   rowTwo: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   rowThree: ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
-  rowFour: ['GUESS WORD'],
+  rowFour: ['Backspace', 'GUESS WORD'],
 };
 
 const keyboard = document.querySelector('.keyboard');
@@ -52,9 +56,20 @@ function displayKeys(rows) {
       keyButton.className = 'key';
       keyButton.textContent = key;
       keyButton.addEventListener('click', (e) => {
-        currentGuess.length < 5 && currentGuess.push(e.target.textContent);
-        insertLetterIntoBox(currentRow, currentGuess);
-        console.log(currentGuess);
+        switch (e.target.textContent) {
+          case 'Backspace':
+            currentGuess.pop();
+            updateRow(currentRow, currentGuess);
+            break;
+          case 'GUESS WORD':
+            console.log(currentGuess);
+            //submitGuess(currentGuess);
+            break;
+          default:
+            currentGuess.length < 5 && currentGuess.push(e.target.textContent);
+            updateRow(currentRow, currentGuess);
+            console.log(currentGuess);
+        }
       });
 
       rowElement.appendChild(keyButton);
@@ -63,18 +78,3 @@ function displayKeys(rows) {
 }
 
 displayKeys(keyRows);
-
-//function typeLetterIntoBox(letter, row) {
-//  let index = 0;
-//  console.log(letter);
-//  console.log(row[0]);
-//  row[index].textContent = letter;
-//}
-
-function insertLetterIntoBox(row, word) {
-  console.log(row);
-  console.log(word);
-  let index = word.length - 1;
-  console.log(index);
-  row[index].textContent = word[index];
-}
