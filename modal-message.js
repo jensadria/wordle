a37f2510d6fa4094b09aa4c5f128185aafaea40a
Jens Adria <jensadria@gmail.com>
2022-03-23 18:38:17 +0800

    Refactored header and some styling

---
+++ modal-message.js
@@ -0,0 +1,17 @@
+const messageContent = {
+  won: {
+    header: 'Nice!',
+    text: "You've guessed the word! Write a definition for it in the box below",
+    textInputClass: 'show',
+  },
+  lost: {
+    header: 'Bummer!',
+    text: "Too bad you didn't get it!",
+    textInputClass: 'hide',
+  },
+  wordExists: {
+    header: 'Oh No!',
+    text: 'This word actually exists! Made up words only!',
+    textInputClass: 'hide',
+  },
+};
--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 const wordGuessForm = document.getElementById('input');
 const wordGuessField = document.getElementById('guess');
 
-//const randomWordIndex = Math.floor(Math.random() * validWords.length);
-//const randomWord = validWords[randomWordIndex];
-const randomWord = 'HELLO';
+const randomWordIndex = Math.floor(Math.random() * validWords.length);
+const randomWord = validWords[randomWordIndex];
 
 const messageContainer = document.getElementById('message-container');
 const messageHeader = document.getElementById('announce');
 const messageSubText = document.getElementById('sub-text');
-const messageParagraph = document.querySelector('#message-container > p');
+const messageParagraph = document.querySelector('#message-container p');
+const messageForm = document.querySelector('#message-container form');
+const messageWord = document.querySelector('#message-container #the-word');
 
 messageContainer.addEventListener('click', (e) =>
   e.target.classList.add('hide')
 );
 
 let guessNr = 0;
-let randomWordArray = randomWord.split('');
 let currentGuess = [];
 let previouslyUsedLetters = [];
 let correctLetters = [];
+let gameOver = false;
+let gameResult = null;
 let currentRow = document.querySelectorAll(`#row-${guessNr} .box`);
 
 function updateRow(row, word) {
@@ -28,43 +30,14 @@ function updateRow(row, word) {
   });
 }
 
-//function determineMissingLetters() {
-//  guessedLetters.forEach((letter, index) => {
-//    if (previouslyUsedLetters.includes(letter)) {
-//      guessedLetters.splice(index, 1);
-//    }
-//  });
-
-//  console.log(guessedLetters);
-//}
-
-//determineMissingLetters();
-
-//function checkWordAndAddClasses(row, word) {
-//  row.forEach((letterBox, index) => {
-//    letterBox.textContent = word[index];
-
-//    if (word[index] === randomWordArray[index]) {
-//      correctLetters.push(word[index]);
-//      letterBox.className = 'box correct-spot';
-//      letterBox.parentElement.className = 'flip';
-//    } else if (randomWordArray.includes(word[index]) ) {
-//      correctLetters.push(word[index]);
-//      letterBox.className = 'box wrong-spot';
-//      letterBox.parentElement.className = 'flip';
-//    } else {
-//      letterBox.className = 'box not-in-word';
-//      letterBox.parentElement.className = 'flip';
-//    }
-//  });
-
-//  if (word.join('') === randomWord) displayMessage(true);
-//  if (guessNr === 6) displayMessage(false);
-//}
-
-//LOLPO
+const randomWordArrayObjects = randomWord.split('').map((lttr) => {
+  return { letter: lttr, color: null };
+});
+console.log(randomWordArrayObjects.map((obj) => obj.letter));
 
 function checkWordAndAddClasses(row, word) {
+  let randomWordArray = randomWord.split('');
+
   row.forEach((letterBox, index) => {
     letterBox.className = 'box not-in-word';
     letterBox.parentElement.className = 'flip';
@@ -73,6 +46,7 @@ function checkWordAndAddClasses(row, word) {
   row.forEach((letterBox, index) => {
     letterBox.textContent = word[index];
     if (word[index] === randomWord[index]) {
+      correctLetters.push(word[index]);
       letterBox.className = 'box correct-spot';
       letterBox.parentElement.className = 'flip';
       randomWordArray.splice(index, 1, '');
@@ -93,8 +67,17 @@ function checkWordAndAddClasses(row, word) {
     }
   });
 
-  if (word.join('') === randomWord) displayMessage(true);
-  if (guessNr === 6) displayMessage(false);
+  if (word.join('') === randomWord) gameResult = 'won';
+  if (guessNr === 6) gameResult = 'lost';
+
+  console.log(gameResult);
+
+  if (gameResult !== null) endGame(gameResult);
+}
+
+function endGame(result) {
+  displayMessage(result);
+  console.log(result);
 }
 
 function goToNextRow() {
@@ -120,22 +103,25 @@ const messageContent = {
   won: {
     header: 'Nice!',
     text: "You've guessed the word! Write a definition for it in the box below",
-    textInputClass: 'show',
+    textInputClass: 'show-form',
   },
   lost: {
     header: 'Bummer!',
-    text: "Too bad you didn't get it!",
-    textInputClass: 'hide',
+    text: "Too bad you didn't get it! The word",
+    textInputClass: 'hide-form',
   },
   wordExists: {
     header: 'Oh No!',
     text: 'This word actually exists! Made up words only!',
+    textInputClass: 'hide-form',
   },
 };
 
-function displayMessage(state) {
-  messageHeader.textContent = messageContent[state].header;
-  messageSubText.textContent = messageContent[state].text;
+function displayMessage(result) {
+  messageHeader.textContent = messageContent[result].header;
+  messageSubText.textContent = messageContent[result].text;
+  messageForm.className = messageContent[result].textInputClass;
+  messageWord.textContent = randomWord;
 
   messageContainer.classList.remove('hide');
 }
@@ -143,8 +129,18 @@ function displayMessage(state) {
 const keyRows = {
   rowOne: ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   rowTwo: ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-  rowThree: ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
-  rowFour: ['GUESS WORD', 'Backspace'],
+  rowThree: [
+    'Enter',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<i class="fa-solid fa-delete-left"></i>',
+  ],
+  //  rowFour: [ ],
 };
 const keyboard = document.querySelector('.keyboard');
 
@@ -160,14 +156,14 @@ function displayKeys(rows) {
     for (const key of rows[keyRow]) {
       const keyButton = document.createElement('div');
       keyButton.className = 'key';
-      keyButton.textContent = key;
+      keyButton.innerHTML = key;
       keyButton.addEventListener('click', (e) => {
-        switch (e.target.textContent) {
-          case 'Backspace':
+        switch (e.target.innerHTML) {
+          case '<i class="fa-solid fa-delete-left"></i>':
             currentGuess.pop();
             updateRow(currentRow, currentGuess);
             break;
-          case 'GUESS WORD':
+          case 'Enter':
             //checkIfValid();
             //if (!valid) break;
             if (currentGuess.length < 5) break;
